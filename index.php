75ad72223f64437628f4a12554ad3a415e01f379
Lazada404 <91785501+Lazada404@users.noreply.github.com>
2021-10-02 18:53:54 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
  *
  * An open source application development framework for PHPs
  *
- * This content is released under the MIT License (MIT)
+ * This content is releeased under the MIT License (MIT)
  *
  * Copyright (c) 2014 - 2019, British Columbia Institute of Technology
  *
